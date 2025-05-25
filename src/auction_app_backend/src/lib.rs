0532a3749ab0e249920ce03f385e782599e3d162
smart_con_tractor <smart_con_tractor@DESKTOP-9EPSJQM.>
2025-05-25 09:02:45 +0200

    Add basic create/view functions.

--- src/auction_app_backend/src/lib.rs
+++ src/auction_app_backend/src/lib.rs
@@ -1,13 +1,12 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use candid::{CandidType, Decode, Encode};
-use ic_stable_structures::memory_manager::{MemoryId, MemoryManager,
-     VirtualMemory};
-use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap,
-     storable::Bound, Storable};
-use std::{borrow::Cow, cell::RefCell};
+use candid::{CandidType, Decode, Encode, Principal};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap, Storable, storable::Bound};
+use std::cell::RefCell;
 use serde::Deserialize;
+use std::borrow::Cow;
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 
@@ -17,7 +16,7 @@ struct Listing {
     description: String,
     starting_price: u64,
     sold: bool,
-    owner: candid::Principal,
+    owner: Principal,
 }
 
 #[derive(CandidType, Deserialize, Clone)]
@@ -28,26 +27,57 @@ struct CreateListing {
 }
 
 impl Storable for Listing {
-    type Id = MemoryId;
-
-    fn id(&self) -> Self::Id {
-        MemoryId::from(self.title.as_bytes())
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(candid::Encode!(&self).unwrap())
     }
 
-    fn encode(&self) -> Vec<u8> {
-        Encode!(&self).unwrap()
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        candid::Decode!(bytes.as_ref(), Listing).unwrap()
     }
 
-    fn decode(bytes: &[u8]) -> Self {
-        Decode!(&bytes).unwrap()
-    }
+    const BOUND: Bound = Bound::Unbounded;
 }
 
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
-    static LISTING_MAP: RefCell<StableBTreeMap<u64, Proposal, Memory>> =
+    static LISTING_MAP: RefCell<StableBTreeMap<u64, Listing, Memory>> =
         RefCell::new(StableBTreeMap::init(MEMORY_MANAGER.with(
             |m| m.borrow().get(MemoryId::new(0)))));
+
+    static LISTING_COUNTER: RefCell<u64> = RefCell::new(0);
+}
+
+#[ic_cdk::query]
+fn get_listing(id: u64) -> Option<Listing> {
+    LISTING_MAP.with(|p| p.borrow().get(&id))
+}
+
+#[ic_cdk::query]
+fn get_listing_count() -> u64 {
+    LISTING_MAP.with(|p| p.borrow().len() as u64)
+}
+
+#[ic_cdk::update]
+fn create_listing(input: CreateListing) -> Listing {
+    let listing = Listing {
+        title: input.title,
+        description: input.description,
+        starting_price: input.starting_price,
+        sold: false,
+        owner: ic_cdk::caller(),
+    };
+
+    LISTING_COUNTER.with(|counter| {
+        let mut id = counter.borrow_mut();
+        let key = *id;
+        *id += 1;
+
+        LISTING_MAP.with(|p| {
+            p.borrow_mut().insert(key, listing.clone());
+        });
+
+        listing
+    })
 }
